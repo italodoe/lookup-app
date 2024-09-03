f34fd0007697eeaa5b9976b0b894646dcf92624c
Italo Castillo <italogitdoe@gmail.com>
2024-09-04 00:31:44 +0200

    add theme by env

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { env } from "./env"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const defaultTheme = ()  => env('NEXT_PUBLIC_COLOR_THEME_NAME')
