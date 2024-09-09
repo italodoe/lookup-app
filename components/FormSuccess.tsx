3ac1534e03542d3490704c4c75a3521b8efdc3b8
Italo Castillo <italogitdoe@gmail.com>
2024-09-09 23:46:53 +0200

    add login form

---
+++ components/FormSuccess.tsx
@@ -0,0 +1,18 @@
+import { CheckCircledIcon } from "@radix-ui/react-icons";
+import { CheckCheckIcon } from "lucide-react";
+import React from "react";
+interface FormErrorProps {
+  message?: string;
+}
+
+const FormSuccess = ({ message }: FormErrorProps) => {
+  if (!message) return null;
+  return (
+    <div className="bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-emerald">
+      <CheckCircledIcon className="h-4 w-4" />
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default FormSuccess;
--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+
 import CardWrapper from "./CardWrapper";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "../ui/input";
+
+import { LoginSchema } from "@/schemas";
+import FormError from "../FormError";
+import FormSuccess from "../FormSuccess";
+
 const LoginForm = () => {
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values);
+    setError("");
+    setSuccess("");
+  };
+
   return (
     <CardWrapper
       headerLabel={"Welcome back"}
@@ -9,7 +48,56 @@ const LoginForm = () => {
       backButtonHref={"/auth/register"}
       showSocial
     >
-        body
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="jon@example.org"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="******" type="password" />
+                  </FormControl>
+                  <Button
+                    size={"sm"}
+                    variant="link"
+                    asChild
+                    className="px-0  font-normal"
+                  >
+                    <Link href="/auth/reset">Forgot password?</Link>
+                  </Button>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
