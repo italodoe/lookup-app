4fbf0bf1c3ffa48c34d1e508ecec7ffe136e1645
Italo Castillo <italogitdoe@gmail.com>
2024-09-11 00:43:01 +0200

    fix break word

--- app/(protected)/dashboard/(home)/layout.tsx
+++ app/(protected)/dashboard/(home)/layout.tsx
@@ -9,7 +9,7 @@ const HomeLayout = ({ children }: { children: ReactNode }) => {
       <div className="flex ">
         <Sidebar />
         <section className="bg-default-2 rounded-3xl flex  flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14 mx-5 max-sm:mb-4 min-h-[calc(100vh-3rem)] shadow hover:shadow-md">
-          <div className="w-full ">{children}</div>
+          <div className="w-full break-all">{children}</div>
         </section>
       </div>
     </div>
