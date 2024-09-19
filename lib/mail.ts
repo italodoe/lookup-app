d3cfa09cb8dd1f90b33579cade26b42d534545eb
Italo Castillo <italogitdoe@gmail.com>
2024-09-19 21:35:15 +0200

    change env constant

--- lib/mail.ts
+++ lib/mail.ts
@@ -8,8 +8,8 @@ import { env } from "./env";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const protocol = env("PROTOCOL");
-  const hostname = env("HOSTNAME");
+  const protocol = env("NEXT_PUBLIC_PROTOCOL");
+  const hostname = env("NEXT_PUBLIC_BASE_URL");
   const default_from = env("MAILER_DEFAULT_FROM");
   const resetLink = `${protocol}://${hostname}${DEFAULT_NEW_PASSWORD_PAGE}?token=${token}`;
 
@@ -22,8 +22,8 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 };
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const protocol = env("PROTOCOL");
-  const hostname = env("HOSTNAME");
+  const protocol = env("NEXT_PUBLIC_PROTOCOL");
+  const hostname = env("NEXT_PUBLIC_BASE_URL");
   const default_from = env("MAILER_DEFAULT_FROM");
   const confirmLink = `${protocol}://${hostname}${DEFAULT_AUTH_NEW_VERIFICATION_PAGE}?token=${token}`;
 
