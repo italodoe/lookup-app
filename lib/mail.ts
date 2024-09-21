e7236d218edfa6074873c9661ed43a1d62913ba1
Italo Castillo <italogitdoe@gmail.com>
2024-09-22 00:34:44 +0200

    refactoring

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,38 +1,45 @@
+import { Resend } from "resend";
 import {
   DEFAULT_AUTH_NEW_VERIFICATION_PAGE,
   DEFAULT_NEW_PASSWORD_PAGE,
 } from "@/routes";
-import { Resend } from "resend";
 import { env } from "./env";
-
 
 const resend = new Resend(env("RESEND_API_KEY"));
 
-export const sendPasswordResetEmail = async (email: string, token: string) => {
+export async function sendPasswordResetEmail(email: string, token: string) {
   const protocol = env("NEXT_PUBLIC_PROTOCOL");
   const hostname = env("NEXT_PUBLIC_BASE_URL");
   const default_from = env("MAILER_DEFAULT_FROM");
   const resetLink = `${protocol}://${hostname}${DEFAULT_NEW_PASSWORD_PAGE}?token=${token}`;
 
-  await resend.emails.send({
+  const { data, error } = await resend.emails.send({
     from: default_from,
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`,
   });
-};
+  if (error) {
+    return { error: error.message };
+  }
+  return { data: data };
+}
 
-export const sendVerificationEmail = async (email: string, token: string) => {
+export async function sendVerificationEmail(email: string, token: string) {
   const protocol = env("NEXT_PUBLIC_PROTOCOL");
   const hostname = env("NEXT_PUBLIC_BASE_URL");
   const default_from = env("MAILER_DEFAULT_FROM");
   const confirmLink = `${protocol}://${hostname}${DEFAULT_AUTH_NEW_VERIFICATION_PAGE}?token=${token}`;
 
-  await resend.emails.send({
+  const { data, error } = await resend.emails.send({
     from: default_from,
     to: email,
     subject: "Confirm your email",
     // todo: improve mail
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm.</p>`,
   });
-};
+  if (error) {
+    return { error: error.message };
+  }
+  return { data: data };
+}
